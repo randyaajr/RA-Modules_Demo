eaa77cc72a49ad2f1979070f43c9132e83abc6a3
randyaajr <67880000+randyaajr@users.noreply.github.com>
2020-11-13 13:37:52 -0500

    This page demonstrates importing functions from another page

---
+++ main.py
@@ -0,0 +1,31 @@
+from lib_num import conversion
+
+from colorama import Fore
+
+input_num = input('Enter the a numeric value: ')
+con_from = input('Which unit of measurement do you wish to converted from:  ')
+con_to = input('Which unit of measurement do you want it converted to: ')
+
+"""
+This simple measurement coversion app coverts the following below:
+
+        "cm" to "m":
+
+        "mm" to "cm":
+
+        "m" to "cm":
+
+        "cm" to "mm":
+
+        "mm" to "m":
+
+        "m" to "mm":
+
+        "ft" to "cm":
+
+        "ft" to "inch":
+
+        "inch" to "cm":
+
+        "inch" to "mm":
+"""
